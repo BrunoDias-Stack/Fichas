3fda4c545f5bd1628e721620800fc59f1ce65f62
BrunoDias-Stack <brunoapdias123@gmail.com>
2021-10-12 22:07:11 -0300

    Criando pasta files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fichas
+Bradesco Fichas
--- controllers/fichasController.js
+++ controllers/fichasController.js
@@ -1,27 +1,23 @@
 const dados = require("../data/dados")
 const saveData = require("../utils/saveData")
 const campos = require("../campos")
+const fs = require('fs');
+const dir = './files';
 
 module.exports = {
     async F1(req, res){
-        res.render('F1')
-        // console.log(req.body)
-    },
+        res.render('F1');
+    },  
    async save(req, res, next){
-        /* criando um id sequencial, obtendo a
-        quantidade de elementos no array dados e somando + 1 */
-
-        //let id = dados.length + 1;
-        
-        
-        /* montando objeto para seguir padrao dentro do arquivo data/dados.js */
-        //let dado = {campos, ...req.body };
-        
-        /* adcionando objeto criado dentro do array recipes */
+        /* adicionando objeto criado dentro do array dados */
+        fs.readdir(dir, (err, files) => {
+            console.log(files.length);
+            });
         dados.push(campos);   
-    
-        // executando funcao que salva alteracoes dos registros no arquivo dados.js
+        /* executando funcao que salva alteracoes dos registros no arquivo dados.js */
         saveData(dados, 'dados.js');
+
+
     
         res.redirect('/ficha_alimentacao-refeicao');
       },
